852a43db475942b02906be862f0275e6ba71466e
KN4CK3R <admin@oldschoolhack.me>
2016-11-09 15:16:07 +0100

    Moved classes to seperate files.

---
+++ DataExchange/SchemaType.cs
@@ -0,0 +1,46 @@
+﻿namespace ReClassNET.DataExchange
+{
+	public enum SchemaType
+	{
+		None,
+		Padding,
+
+		Array,
+		ClassPtrArray,
+		ClassInstance,
+		Class,
+		ClassPtr,
+		Double,
+		Float,
+		FunctionPtr,
+		Hex8,
+		Hex16,
+		Hex32,
+		Hex64,
+		Int8,
+		Int16,
+		Int32,
+		Int64,
+		Matrix3x3,
+		Matrix3x4,
+		Matrix4x4,
+		UInt8,
+		UInt16,
+		UInt32,
+		UInt64,
+		UTF8Text,
+		UTF8TextPtr,
+		UTF16Text,
+		UTF16TextPtr,
+		UTF32Text,
+		UTF32TextPtr,
+		Vector2,
+		Vector3,
+		Vector4,
+		VTable,
+		VMethod,
+		BitField,
+
+		Custom
+	}
+}
