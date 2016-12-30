330f154a74b257515d82bebcab7ad2a432cbc3a5
KN4CK3R <admin@oldschoolhack.me>
2016-12-30 22:18:34 +0100

    Reduced custom filesystem code.

--- NativeCore/Unix/EnumerateProcesses.cpp
+++ NativeCore/Unix/EnumerateProcesses.cpp
@@ -2,136 +2,45 @@
 #include <string>
 #include <sstream>
 #include <fstream>
+#include <experimental/filesystem>
 
 #include "NativeCore.hpp"
 
+namespace fs = std::experimental::filesystem;
+
 // std::filesystem library doesn't work @Ubuntu 16.10, read_symlink() always fails.
-//#define USE_STD_FILESYSTEM
-#ifdef USE_STD_FILESYSTEM
-#include <experimental/filesystem>
-#else
-#include <dirent.h>
-#include <sys/stat.h>
+#define USE_CUSTOM_READ_SYMLINK
+
+#ifdef USE_CUSTOM_READ_SYMLINK
 #include <unistd.h>
 
-class path
+fs::path my_read_symlink(const fs::path& p, std::error_code& ec)
 {
-public:
-	path() = default;
+	fs::path symlink_path;
 
-	path(const char* _path)
-		: path(std::string(_path))
-	{
-	}
-
-	path(const std::string& _path)
-		: buf(_path)
-	{
-	}
-
-	path(const path&) = default;
-
-	path& append(const std::string& part)
-	{
-		if (buf.back() != separator)
-		{
-			buf += separator;
-		}
-		buf += part;
-
-		return *this;
-	}
-
-	path& operator/=(const path& p)
-	{
-		append(p.buf);
-
-		return *this;
-	}
-
-	const std::string& string() const
-	{
-		return buf;
-	}
-
-	const char* c_str() const
-	{
-		return buf.c_str();
-	}
-
-private:
-	static const char separator = '/';
-
-	std::string buf;
-};
-
-inline path operator/(const path& lhs, const path& rhs)
-{
-	return path(lhs) /= rhs;
-}
-
-enum class FileType
-{
-	Unknown,
-
-	File,
-	Directory,
-	Symlink
-};
-
-FileType file_type(const path& p)
-{
-	struct stat path_stat = {};
-	if (::lstat(p.c_str(), &path_stat) == 0)
-	{
-		if (S_ISREG(path_stat.st_mode))
-		{
-			return FileType::File;
-		}
-		else if (S_ISDIR(path_stat.st_mode))
-		{
-			return FileType::Directory;
-		}
-		else if (S_ISLNK(path_stat.st_mode))
-		{
-			return FileType::Symlink;
-		}
-	}
-
-	return FileType::Unknown;
-}
-
-inline bool is_directory(const path& p)
-{
-	return file_type(p) == FileType::Directory;
-}
-
-inline bool is_symlink(const path& p)
-{
-	return file_type(p) == FileType::Symlink;
-}
-
-bool read_symlink(const path& p, path& out_p)
-{
 	std::string temp(64, '\0');
-
 	for (;; temp.resize(temp.size() * 2))
 	{
 		ssize_t result;
 		if ((result = ::readlink(p.c_str(), /*temp.data()*/ &temp[0], temp.size())) == -1)
 		{
-			return false;
+			ec.assign(errno, std::system_category());
+			break;
 		}
 		else
 		{
 			if (result != (ssize_t)temp.size())
 			{
-				out_p = path(std::string(temp.begin(), temp.begin() + result));
+				symlink_path = fs::path(std::string(temp.begin(), temp.begin() + result));
 
-				return true;
+				ec.clear();
+
+				break;
 			}
 		}
 	}
+
+	return symlink_path;
 }
 
 #endif
@@ -194,57 +103,33 @@ extern "C" void EnumerateProcesses(EnumerateProcessCallback callbackProcess)
 		return;
 	}
 
-#ifdef USE_STD_FILESYSTEM
-	using namespace std::experimental::filesystem;
-	using namespace std;
-
-	path procPath("/proc");
-	if (is_directory(procPath))
+	fs::path procPath("/proc");
+	if (fs::is_directory(procPath))
 	{
-		for (auto& d : directory_iterator(procPath))
+		for (auto& d : fs::directory_iterator(procPath))
 		{
-			if (is_directory(d))
+			if (fs::is_directory(d))
 			{
-				auto pidPath = d.path();
+				auto processPath = d.path();
 
 				auto name = processPath.filename().string();
 				if (is_number(name))
 				{
 					auto exeSymLink = processPath / "exe";
-					if (is_symlink(symlink_status(exeSymLink)))
+					if (fs::is_symlink(fs::symlink_status(exeSymLink)))
 					{
-						error_code ec;
-						auto linkPath = read_symlink(exeSymLink, ec).string();
+						std::error_code ec;
+						auto linkPath = 
+#ifdef USE_CUSTOM_READ_SYMLINK
+							my_read_symlink
+#else
+							read_symlink
+#endif
+							(exeSymLink, ec).string();
 						if (!ec)
 						{
-#else
-	path procPath("/proc");
-	if (is_directory(procPath))
-	{
-		auto directory = opendir(procPath.c_str());
-		if (directory == nullptr)
-		{
-			return;
-		}
+							auto auxvPath = processPath / "auxv";
 
-		struct dirent *entry;
-		while ((entry = readdir(directory)) != nullptr)
-		{
-			auto pidPath = procPath / entry->d_name;
-			if (is_directory(pidPath))
-			{
-				std::string name(entry->d_name);
-				if (is_number(name))
-				{
-					auto exePath = pidPath / "exe";
-					if (is_symlink(exePath))
-					{
-						path linkPath;
-						if (read_symlink(exePath, linkPath))
-						{
-#endif
-							auto auxvPath = pidPath / "auxv";
-							
 							auto platform = GetProcessPlatform(auxvPath.string());
 #ifdef NATIVE_CORE_64
 							if (platform == Platform::X64)
