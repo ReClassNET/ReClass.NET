d03b9803e947598d8734e9879ae856f685b58426
KN4CK3R <admin@oldschoolhack.me>
2017-02-26 12:49:59 +0100

    Added ValueTypeWrapper.

---
+++ Util/ValueTypeWrapper.cs
@@ -0,0 +1,19 @@
+﻿namespace ReClassNET.Util
+{
+	/// <summary>A wrapper for non reference types.</summary>
+	/// <typeparam name="T">Generic type parameter.</typeparam>
+	public class ValueTypeWrapper<T> where T : struct
+	{
+		public ValueTypeWrapper(T value)
+		{
+			Value = value;
+		}
+
+		public T Value { get; set; }
+
+		public static implicit operator ValueTypeWrapper<T>(T value)
+		{
+			return new ValueTypeWrapper<T>(value);
+		}
+	}
+}
