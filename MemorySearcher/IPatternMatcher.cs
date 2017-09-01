c65900199e84dfc87d896d551af34da407a4e86c
KN4CK3R <admin@oldschoolhack.me>
2017-09-02 00:30:41 +0200

    Added ContractClass.

--- MemorySearcher/IPatternMatcher.cs
+++ MemorySearcher/IPatternMatcher.cs
@@ -1,11 +1,37 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 
 namespace ReClassNET.MemorySearcher
 {
+	[ContractClass(typeof(PatternMatcherContract))]
 	public interface IPatternMatcher
 	{
 		IEnumerable<int> SearchMatches(IList<byte> data);
 
 		IEnumerable<int> SearchMatches(IList<byte> data, int index, int count);
+	}
+
+	[ContractClassFor(typeof(IPatternMatcher))]
+	internal abstract class PatternMatcherContract : IPatternMatcher
+	{
+		public IEnumerable<int> SearchMatches(IList<byte> data)
+		{
+			Contract.Requires(data != null);
+			Contract.Ensures(Contract.Result<IEnumerable<int>>() != null);
+
+			throw new NotImplementedException();
+		}
+
+		public IEnumerable<int> SearchMatches(IList<byte> data, int index, int count)
+		{
+			Contract.Requires(data != null);
+			Contract.Requires(index >= 0);
+			Contract.Requires(count >= 0);
+			Contract.Requires(data.Count - index >= count);
+			Contract.Ensures(Contract.Result<IEnumerable<int>>() != null);
+
+			throw new NotImplementedException();
+		}
 	}
 }
