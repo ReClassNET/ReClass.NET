4484bbb269c2e9f81fb481b7ba2e26c4e3a538d4
KN4CK3R <admin@oldschoolhack.me>
2017-09-25 22:44:49 +0200

    Added KeyboardInput.

---
+++ Input/KeyboardInput.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using System.Windows.Forms;
+
+namespace ReClassNET.Input
+{
+	public class KeyboardInput : IDisposable
+	{
+		private readonly IntPtr handle;
+
+		public KeyboardInput()
+		{
+			handle = Program.CoreFunctions.InitializeInput();
+		}
+
+		public void Dispose()
+		{
+			ReleaseUnmanagedResources();
+
+			GC.SuppressFinalize(this);
+		}
+
+		~KeyboardInput()
+		{
+			ReleaseUnmanagedResources();
+		}
+
+		private void ReleaseUnmanagedResources()
+		{
+			Program.CoreFunctions.ReleaseInput(handle);
+		}
+
+		public Keys[] GetPressedKeys()
+		{
+			Contract.Ensures(Contract.Result<Keys[]>() != null);
+
+			return Program.CoreFunctions.GetPressedKeys(handle);
+		}
+	}
+}
