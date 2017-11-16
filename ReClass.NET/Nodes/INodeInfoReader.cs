75359bbeb46add26344cadd910ba465924b47114
KN4CK3R <admin@oldschoolhack.me>
2017-11-17 00:48:47 +0100

    Changed parameter name.

--- ReClass.NET/Nodes/INodeInfoReader.cs
+++ ReClass.NET/Nodes/INodeInfoReader.cs
@@ -1,10 +1,31 @@
 ﻿using System;
+using System.Diagnostics.Contracts;
 using ReClassNET.Memory;
 
 namespace ReClassNET.Nodes
 {
+	[ContractClass(typeof(NodeInfoReaderContract))]
 	public interface INodeInfoReader
 	{
-		string ReadNodeInfo(BaseNode node, IntPtr value, MemoryBuffer memory);
+		/// <summary>
+		/// Used to print custom informations about the node.
+		/// </summary>
+		/// <param name="node">The current node.</param>
+		/// <param name="nodeAddress">The absolute address of the node.</param>
+		/// <param name="memory">The current <see cref="MemoryBuffer"/>.</param>
+		/// <returns>Custom informations about the node or null.</returns>
+		string ReadNodeInfo(BaseNode node, IntPtr nodeAddress, MemoryBuffer memory);
+	}
+
+	[ContractClassFor(typeof(INodeInfoReader))]
+	internal abstract class NodeInfoReaderContract : INodeInfoReader
+	{
+		public string ReadNodeInfo(BaseNode node, IntPtr nodeAddress, MemoryBuffer memory)
+		{
+			Contract.Requires(node != null);
+			Contract.Requires(memory != null);
+
+			throw new NotImplementedException();
+		}
 	}
 }
