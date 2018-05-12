ReClassNET/ReClass.NET

commit 566ae12907c50d62c19dcfd6f6a1356260e6cb02

Implemented double click to expand nodes. (fixed #30)

ReClass.NET/UI/MemoryViewControl.cs
@@ -451,17 +451,29 @@ namespace ReClassNET.UI
 
 			editBox.Visible = false;
 
-			foreach (var hotSpot in hotSpots.Where(h => h.Type == HotSpotType.Edit))
+			foreach (var hotSpot in hotSpots.Where(h => h.Type == HotSpotType.Edit || h.Type ==  HotSpotType.Select))
 			{
 				if (hotSpot.Rect.Contains(e.Location))
 				{
-					editBox.BackColor = Program.Settings.SelectedColor;
-					editBox.HotSpot = hotSpot;
-					editBox.Visible = true;
+					if (hotSpot.Type == HotSpotType.Edit)
+					{
+						editBox.BackColor = Program.Settings.SelectedColor;
+						editBox.HotSpot = hotSpot;
+						editBox.Visible = true;
 
-					editBox.ReadOnly = hotSpot.Id == HotSpot.ReadOnlyId;
+						editBox.ReadOnly = hotSpot.Id == HotSpot.ReadOnlyId;
 
-					break;
+						break;
+					}
+
+					if (hotSpot.Type == HotSpotType.Select)
+					{
+						hotSpot.Node.ToggleLevelOpen(hotSpot.Level);
+
+						Invalidate();
+
+						break;
+					}
 				}
 			}
 		}

commit a41ad33dbda07928effe18c4807d9624d04494d1

Fixed bug introduced with 566ae12.

ReClass.NET/UI/MemoryViewControl.cs
@@ -451,6 +451,9 @@ namespace ReClassNET.UI
 
 			editBox.Visible = false;
 
+			BaseNode toggleNode = null;
+			var level = 0;
+
 			foreach (var hotSpot in hotSpots.Where(h => h.Type == HotSpotType.Edit || h.Type ==  HotSpotType.Select))
 			{
 				if (hotSpot.Rect.Contains(e.Location))
@@ -463,18 +466,22 @@ namespace ReClassNET.UI
 
 						editBox.ReadOnly = hotSpot.Id == HotSpot.ReadOnlyId;
 
-						break;
+						return;
 					}
 
 					if (hotSpot.Type == HotSpotType.Select)
 					{
-						hotSpot.Node.ToggleLevelOpen(hotSpot.Level);
-
-						Invalidate();
-
-						break;
+						toggleNode = hotSpot.Node;
+						level = hotSpot.Level;
 					}
 				}
+			}
+
+			if (toggleNode != null)
+			{
+				toggleNode.ToggleLevelOpen(level);
+
+				Invalidate();
 			}
 		}
 