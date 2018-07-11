ReClassNET/ReClass.NET

commit 37f91bb473eeb8b2d2af32719d024022260a3806

Disassemble multiple instructions at once.

ReClass.NET/Memory/Disassembler.cs
@@ -71,7 +71,15 @@ namespace ReClassNET.Memory
 
 			while (eip.CompareTo(end) == -1)
 			{
-				var res = coreFunctions.DisassembleCode(eip, end.Sub(eip).ToInt32() + 1, virtualAddress, false, out var instruction);
+				var instruction = default(InstructionData);
+
+				// Grab only one instruction.
+				var res = coreFunctions.DisassembleCode(eip, end.Sub(eip).ToInt32() + 1, virtualAddress, false, (ref InstructionData data) =>
+				{
+					instruction = data;
+
+					return false;
+				});
 				if (!res)
 				{
 					break;
@@ -170,64 +178,40 @@ namespace ReClassNET.Memory
 		/// <returns>The previous instruction.</returns>
 		private DisassembledInstruction GetPreviousInstruction(IntPtr address, IntPtr virtualAddress)
 		{
-			var end = address + 80;
+			var instruction = default(InstructionData);
 
-			var instruction = new InstructionData();
-
-			var x = GetPreviousInstructionHelper(end, 80, virtualAddress, ref instruction);
-			if (x != end)
+			foreach (var offset in new[] { 80, 40, 20, 10, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 })
 			{
-				x = GetPreviousInstructionHelper(end, 40, virtualAddress, ref instruction);
-				if (x != end)
+				var currentAddress = address - offset;
+
+				coreFunctions.DisassembleCode(currentAddress, offset + 1, virtualAddress - offset, false, (ref InstructionData data) =>
 				{
-					x = GetPreviousInstructionHelper(end, 20, virtualAddress, ref instruction);
-					if (x != end)
+					var nextAddress = currentAddress + data.Length;
+					if (nextAddress.CompareTo(address) > -1)
 					{
-						x = GetPreviousInstructionHelper(end, 10, virtualAddress, ref instruction);
-						if (x != end)
-						{
-							for (var i = 1; i < 15; ++i)
-							{
-								x = address + 65 + i;
-								if (coreFunctions.DisassembleCode(x, end.Sub(x).ToInt32() + 1, virtualAddress, false, out instruction))
-								{
-									if (x + instruction.Length == end)
-									{
-										break;
-									}
-								}
-							}
-						}
+						return false;
 					}
+
+					instruction = data;
+
+					currentAddress = nextAddress;
+
+					return true;
+				});
+
+				if (currentAddress == address)
+				{
+					return new DisassembledInstruction
+					{
+						Address = virtualAddress - instruction.Length,
+						Length = instruction.Length,
+						Data = instruction.Data,
+						Instruction = instruction.Instruction
+					};
 				}
 			}
 
-			return new DisassembledInstruction
-			{
-				Address = virtualAddress - instruction.Length,
-				Length = instruction.Length,
-				Data = instruction.Data,
-				Instruction = instruction.Instruction
-			};
-		}
-
-		private IntPtr GetPreviousInstructionHelper(IntPtr address, int distance, IntPtr virtualAddress, ref InstructionData instruction)
-		{
-			var x = address - distance;
-			var y = virtualAddress - distance;
-			while (x.CompareTo(address) == -1) // aka x < address
-			{
-				if (coreFunctions.DisassembleCode(x, address.Sub(x).ToInt32() + 1, y, false, out instruction))
-				{
-					x += instruction.Length;
-					y += instruction.Length;
-				}
-				else
-				{
-					break;
-				}
-			}
-			return x;
+			return null;
 		}
 
 		/// <summary>Tries to find the start address of the function <paramref name="address"/> points into.</summary>

NativeCore/Shared/DistormHelper.cpp
@@ -7,18 +7,18 @@ extern "C"
 #include <../src/instructions.h>
 }
 
-bool AreOperandsStatic(_DInst *instruction, const int prefixLength)
+bool AreOperandsStatic(const _DInst &instruction, const int prefixLength)
 {
-	const auto fc = META_GET_FC(instruction->meta);
+	const auto fc = META_GET_FC(instruction.meta);
 	if (fc == FC_UNC_BRANCH || fc == FC_CND_BRANCH)
 	{
-		if (instruction->size - prefixLength < 5)
+		if (instruction.size - prefixLength < 5)
 		{
 			return true;
 		}
 	}
 
-	const auto ops = instruction->ops;
+	const auto ops = instruction.ops;
 	for (auto i = 0; i < OPERANDS_NO; i++)
 	{
 		switch (ops[i].type)
@@ -37,7 +37,7 @@ bool AreOperandsStatic(_DInst *instruction, const int prefixLength)
 		case O_DISP:
 		case O_SMEM:
 		case O_MEM:
-			if (instruction->dispSize < 32)
+			if (instruction.dispSize < 32)
 			{
 				continue;
 			}
@@ -58,12 +58,12 @@ bool AreOperandsStatic(_DInst *instruction, const int prefixLength)
 	return true;
 }
 
-int GetStaticInstructionBytes(_DInst *instruction, const uint8_t *data)
+int GetStaticInstructionBytes(const _DInst &instruction, const uint8_t *data)
 {
 	_CodeInfo info = {};
 	info.codeOffset = reinterpret_cast<_OffsetType>(data);
 	info.code = data;
-	info.codeLen = instruction->size;
+	info.codeLen = instruction.size;
 	info.features = DF_NONE;
 #ifdef RECLASSNET32
 	info.dt = Decode32Bits;
@@ -88,13 +88,13 @@ int GetStaticInstructionBytes(_DInst *instruction, const uint8_t *data)
 
 	if (AreOperandsStatic(instruction, prefixLength))
 	{
-		return instruction->size;
+		return instruction.size;
 	}
 
-	return instruction->size - info.codeLen;
+	return instruction.size - info.codeLen;
 }
 
-bool DisassembleCodeImpl(const RC_Pointer address, const RC_Size length, const RC_Pointer virtualAddress, const bool determineStaticInstructionBytes, InstructionData* instruction)
+_CodeInfo CreateCodeInfo(const RC_Pointer address, const RC_Size length, const RC_Pointer virtualAddress)
 {
 	_CodeInfo info = {};
 	info.codeOffset = reinterpret_cast<_OffsetType>(virtualAddress);
@@ -108,34 +108,27 @@ bool DisassembleCodeImpl(const RC_Pointer address, const RC_Size length, const R
 	info.dt = Decode64Bits;
 #endif
 
-	_DInst decodedInstructions[1] = {};
-	unsigned int instructionCount = 0;
+	return info;
+}
 
-	const auto res = distorm_decompose(&info, decodedInstructions, 1, &instructionCount);
-	if (res == DECRES_INPUTERR || !(res == DECRES_SUCCESS || res == DECRES_MEMORYERR) || instructionCount != 1)
-	{
-		return false;
-	}
-
-	_DecodedInst instructionInfo = {};
-	distorm_format(&info, &decodedInstructions[0], &instructionInfo);
-
-	instruction->Length = instructionInfo.size;
-	std::memcpy(instruction->Data, address, instructionInfo.size);
+void FillInstructionData(const RC_Pointer address, const _DInst& instruction, const _DecodedInst& instructionInfo, const bool determineStaticInstructionBytes, InstructionData* data)
+{
+	data->Length = instructionInfo.size;
+	std::memcpy(data->Data, address, instructionInfo.size);
 
 	MultiByteToUnicode(
 		reinterpret_cast<const char*>(instructionInfo.mnemonic.p),
-		instruction->Instruction,
+		data->Instruction,
 		instructionInfo.mnemonic.length
 	);
 	if (instructionInfo.operands.length != 0)
 	{
-		instruction->Instruction[instructionInfo.mnemonic.length] = ' ';
+		data->Instruction[instructionInfo.mnemonic.length] = ' ';
 
 		MultiByteToUnicode(
 			reinterpret_cast<const char*>(instructionInfo.operands.p),
 			0,
-			instruction->Instruction,
+			data->Instruction,
 			instructionInfo.mnemonic.length + 1,
 			std::min<int>(64 - 1 - instructionInfo.mnemonic.length, instructionInfo.operands.length)
 		);
@@ -143,15 +136,57 @@ bool DisassembleCodeImpl(const RC_Pointer address, const RC_Size length, const R
 
 	if (determineStaticInstructionBytes)
 	{
-		instruction->StaticInstructionBytes = GetStaticInstructionBytes(
-			&decodedInstructions[0],
+		data->StaticInstructionBytes = GetStaticInstructionBytes(
+			instruction,
 			reinterpret_cast<const uint8_t*>(address)
 		);
 	}
 	else
 	{
-		instruction->StaticInstructionBytes = -1;
+		data->StaticInstructionBytes = -1;
 	}
+}
 
-	return true;
+bool DisassembleInstructionsImpl(const RC_Pointer address, const RC_Size length, const RC_Pointer virtualAddress, const bool determineStaticInstructionBytes, EnumerateInstructionCallback callback)
+{
+	auto info = CreateCodeInfo(address, length, virtualAddress);
+
+	const unsigned MaxInstructions = 50;
+
+	_DInst decodedInstructions[MaxInstructions] = {};
+	unsigned count = 0;
+
+	while (true)
+	{
+		const auto res = distorm_decompose(&info, decodedInstructions, MaxInstructions, &count);
+		if (res == DECRES_INPUTERR)
+		{
+			return false;
+		}
+
+		for (auto i = 0u; i < count; ++i)
+		{
+			_DecodedInst instructionInfo = {};
+			distorm_format(&info, &decodedInstructions[i], &instructionInfo);
+
+			InstructionData data;
+			FillInstructionData(address, decodedInstructions[i], instructionInfo, determineStaticInstructionBytes, &data);
+
+			if (callback(&data) == false)
+			{
+				return true;
+			}
+		}
+
+		if (res == DECRES_SUCCESS || count == 0)
+		{
+			return true;
+		}
+
+		const auto offset = decodedInstructions[count - 1].addr - info.codeOffset;
+
+		info.codeOffset += offset;
+		info.code += offset;
+		info.codeLen -= offset;
+	}
 }

NativeCore/Shared/DistormHelper.hpp
@@ -2,4 +2,6 @@
 
 #include "../ReClassNET_Plugin.hpp"
 
-bool DisassembleCodeImpl(const RC_Pointer address, const RC_Size length, const RC_Pointer virtualAddress, const bool determineStaticInstructionBytes, InstructionData* instruction);
+typedef bool(RC_CallConv EnumerateInstructionCallback)(InstructionData* data);
+
+bool DisassembleInstructionsImpl(const RC_Pointer address, const RC_Size length, const RC_Pointer virtualAddress, const bool determineStaticInstructionBytes, EnumerateInstructionCallback callback);

NativeCore/Unix/DisassembleCode.cpp
@@ -1,6 +1,6 @@
 #include "../Shared/DistormHelper.hpp"
 
-extern "C" bool RC_CallConv DisassembleCode(RC_Pointer address, RC_Size length, RC_Pointer virtualAddress, bool determineStaticInstructionBytes, InstructionData* instruction)
+extern "C" bool RC_CallConv DisassembleCode(RC_Pointer address, RC_Size length, RC_Pointer virtualAddress, bool determineStaticInstructionBytes, EnumerateInstructionCallback callback)
 {
-	return DisassembleCodeImpl(address, length, virtualAddress, determineStaticInstructionBytes, instruction);
+	return DisassembleInstructionsImpl(address, length, virtualAddress, determineStaticInstructionBytes, callback);
 }

NativeCore/Windows/DisassembleCode.cpp
@@ -1,6 +1,6 @@
 #include "../Shared/DistormHelper.hpp"
 
-bool RC_CallConv DisassembleCode(RC_Pointer address, RC_Size length, RC_Pointer virtualAddress, bool determineStaticInstructionBytes, InstructionData* instruction)
+bool RC_CallConv DisassembleCode(RC_Pointer address, RC_Size length, RC_Pointer virtualAddress, bool determineStaticInstructionBytes, EnumerateInstructionCallback callback)
 {
-	return DisassembleCodeImpl(address, length, virtualAddress, determineStaticInstructionBytes, instruction);
+	return DisassembleInstructionsImpl(address, length, virtualAddress, determineStaticInstructionBytes, callback);
 }

ReClass.NET/Core/CoreFunctionsManager.cs
@@ -174,9 +174,9 @@ namespace ReClassNET.Core
 
 		#region Internal Core Functions
 
-		public bool DisassembleCode(IntPtr address, int length, IntPtr virtualAddress, bool determineStaticInstructionBytes, out InstructionData instruction)
+		public bool DisassembleCode(IntPtr address, int length, IntPtr virtualAddress, bool determineStaticInstructionBytes, EnumerateInstructionCallback callback)
 		{
-			return internalCoreFunctions.DisassembleCode(address, length, virtualAddress, determineStaticInstructionBytes, out instruction);
+			return internalCoreFunctions.DisassembleCode(address, length, virtualAddress, determineStaticInstructionBytes, callback);
 		}
 
 		public IntPtr InitializeInput()

ReClass.NET/Core/InternalCoreFunctions.cs
@@ -7,6 +7,8 @@ using ReClassNET.Util;
 
 namespace ReClassNET.Core
 {
+	public delegate bool EnumerateInstructionCallback(ref InstructionData data);
+
 	internal class InternalCoreFunctions : NativeCoreWrapper, IDisposable
 	{
 		private const string CoreFunctionsModuleWindows = "NativeCore.dll";
@@ -15,7 +17,7 @@ namespace ReClassNET.Core
 		private readonly IntPtr handle;
 
 		[return: MarshalAs(UnmanagedType.I1)]
-		private delegate bool DisassembleCodeDelegate(IntPtr address, IntPtr length, IntPtr virtualAddress, [MarshalAs(UnmanagedType.I1)] bool determineStaticInstructionBytes, out InstructionData instruction);
+		private delegate bool DisassembleCodeDelegate(IntPtr address, IntPtr length, IntPtr virtualAddress, [MarshalAs(UnmanagedType.I1)] bool determineStaticInstructionBytes, [MarshalAs(UnmanagedType.FunctionPtr)] EnumerateInstructionCallback callback);
 
 		private delegate IntPtr InitializeInputDelegate();
 
@@ -76,9 +78,9 @@ namespace ReClassNET.Core
 
 		#endregion
 
-		public bool DisassembleCode(IntPtr address, int length, IntPtr virtualAddress, bool determineStaticInstructionBytes, out InstructionData instruction)
+		public bool DisassembleCode(IntPtr address, int length, IntPtr virtualAddress, bool determineStaticInstructionBytes, EnumerateInstructionCallback callback)
 		{
-			return disassembleCodeDelegate(address, (IntPtr)length, virtualAddress, determineStaticInstructionBytes, out instruction);
+			return disassembleCodeDelegate(address, (IntPtr)length, virtualAddress, determineStaticInstructionBytes, callback);
 		}
 
 		public IntPtr InitializeInput()

ReClass.NET/MemoryScanner/PatternScanner.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics.Contracts;
 using System.Runtime.InteropServices;
+using ReClassNET.Core;
 using ReClassNET.Memory;
 using ReClassNET.Util;
 
@@ -105,23 +106,15 @@ namespace ReClassNET.MemoryScanner
 			try
 			{
 				var eip = handle.AddrOfPinnedObject();
-				var end = eip + size;
 
-				while (eip.CompareTo(end) == -1)
+				process.CoreFunctions.DisassembleCode(eip, size, IntPtr.Zero, true, (ref InstructionData instruction) =>
 				{
-					var res = process.CoreFunctions.DisassembleCode(eip, end.Sub(eip).ToInt32() + 1, IntPtr.Zero, true, out var instruction);
-					if (!res)
-					{
-						break;
-					}
-
 					for (var i = 0; i < instruction.Length; ++i)
 					{
 						data.Add(Tuple.Create(instruction.Data[i], i >= instruction.StaticInstructionBytes));
 					}
-
-					eip += instruction.Length;
-				}
+					return true;
+				});
 			}
 			finally
 			{

commit 93c6e22c072768be81bdff47b4760176b66b9320

Fixed errors from #70 introduced with 37f91bb and 8651995.

ReClass.NET/Memory/Disassembler.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics.Contracts;
+using System.Linq;
 using System.Runtime.InteropServices;
 using ReClassNET.Core;
 using ReClassNET.Extensions;
-using ReClassNET.Util;
 
 namespace ReClassNET.Memory
 {
@@ -143,12 +143,16 @@ namespace ReClassNET.Memory
 		/// <returns>The prior instruction.</returns>
 		public DisassembledInstruction RemoteGetPreviousInstruction(RemoteProcess process, IntPtr address)
 		{
-			var buffer = process.ReadRemoteMemory(address - 6 * MaximumInstructionLength, 7 * MaximumInstructionLength);
+			const int TotalBufferSize = 7 * MaximumInstructionLength;
+			const int BufferShiftSize = 6 * MaximumInstructionLength;
+
+			var buffer = process.ReadRemoteMemory(address - BufferShiftSize, TotalBufferSize);
 
 			var handle = GCHandle.Alloc(buffer, GCHandleType.Pinned);
 			try
 			{
 				var bufferAddress = handle.AddrOfPinnedObject();
+				var targetBufferAddress = bufferAddress + BufferShiftSize;
 
 				var instruction = default(InstructionData);
 
@@ -157,16 +161,16 @@ namespace ReClassNET.Memory
 					6 * MaximumInstructionLength,
 					4 * MaximumInstructionLength,
 					2 * MaximumInstructionLength,
-					MaximumInstructionLength,
+					1 * MaximumInstructionLength,
 					14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1
 				})
 				{
-					var currentAddress = bufferAddress - offset;
+					var currentAddress = targetBufferAddress - offset;
 
 					coreFunctions.DisassembleCode(currentAddress, offset + 1, address - offset, false, (ref InstructionData data) =>
 					{
 						var nextAddress = currentAddress + data.Length;
-						if (nextAddress.CompareTo(address) > -1)
+						if (nextAddress.CompareTo(targetBufferAddress) > 0)
 						{
 							return false;
 						}
@@ -178,7 +182,7 @@ namespace ReClassNET.Memory
 						return true;
 					});
 
-					if (currentAddress == address)
+					if (currentAddress == targetBufferAddress)
 					{
 						return new DisassembledInstruction(ref instruction);
 					}
@@ -227,19 +231,9 @@ namespace ReClassNET.Memory
 							if (prevInstruction.Length == 1 && prevInstruction.Data[0] == 0xCC)
 							{
 								// Disassemble the code from the start and check if the instructions sum up to address.
-								var length = 0;
-								var res = coreFunctions.DisassembleCode(start, address.Sub(start).ToInt32(), IntPtr.Zero, false, (ref InstructionData data) =>
-								{
-									length += data.Length;
-
-									return true;
-								});
-								if (!res)
-								{
-									continue;
-								}
-
-								if (start + length == address)
+								var totalInstructionLength = RemoteDisassembleCode(process, start, address.Sub(start).ToInt32())
+									.Sum(ins => ins.Length);
+								if (start + totalInstructionLength == address)
 								{
 									return start;
 								}