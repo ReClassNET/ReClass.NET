3ea90cb82eaa65a6f460b4f37121727e3caf416f
KN4CK3R <admin@oldschoolhack.me>
2018-12-16 01:25:36 +0100

    Use Equals instead operator==.

--- ReClass.NET/Extensions/EncodingExtensions.cs
+++ ReClass.NET/Extensions/EncodingExtensions.cs
@@ -5,14 +5,14 @@ namespace ReClassNET.Extensions
 {
 	public static class EncodingExtension
 	{
-		/// <summary>Gets the (wrong) byte count per character. Special chars may need more bytes per character.</summary>
+		/// <summary>Gets the (perhaps wrong) byte count per character. Special characters may need more bytes.</summary>
 		/// <param name="encoding">The encoding.</param>
 		/// <returns>The byte count per character.</returns>
 		public static int GetSimpleByteCountPerChar(this Encoding encoding)
 		{
-			if (encoding == Encoding.UTF8 || encoding == Encoding.ASCII) return 1;
-			if (encoding == Encoding.Unicode) return 2;
-			if (encoding == Encoding.UTF32) return 4;
+			if (encoding.Equals(Encoding.UTF8) || encoding.Equals(Encoding.ASCII)) return 1;
+			if (encoding.Equals(Encoding.Unicode)) return 2;
+			if (encoding.Equals(Encoding.UTF32)) return 4;
 
 			throw new NotImplementedException();
 		}
