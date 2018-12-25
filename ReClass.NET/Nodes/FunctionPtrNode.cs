fb6a438e8e86ade6390ccb33edb3611257f70e5c
KN4CK3R <admin@oldschoolhack.me>
2018-12-25 11:18:32 +0100

    Removed unused comment.

--- ReClass.NET/Nodes/FunctionPtrNode.cs
+++ ReClass.NET/Nodes/FunctionPtrNode.cs
@@ -5,11 +5,6 @@ namespace ReClassNET.Nodes
 {
 	public class FunctionPtrNode : BaseFunctionPtrNode
 	{
-		/// <summary>Draws this node.</summary>
-		/// <param name="view">The view information.</param>
-		/// <param name="x">The x coordinate.</param>
-		/// <param name="y">The y coordinate.</param>
-		/// <returns>The pixel size the node occupies.</returns>
 		public override Size Draw(ViewInfo view, int x, int y)
 		{
 			return Draw(view, x, y, "FunctionPtr", Name);
