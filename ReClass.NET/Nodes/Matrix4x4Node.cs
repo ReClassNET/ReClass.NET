a50ea42606d975ae52387680acdca3e4ec27dc5a
KN4CK3R <admin@oldschoolhack.me>
2019-01-04 15:41:02 +0100

    Fixed icon.

--- ReClass.NET/Nodes/Matrix4x4Node.cs
+++ ReClass.NET/Nodes/Matrix4x4Node.cs
@@ -51,7 +51,7 @@ namespace ReClassNET.Nodes
 		public override void GetUserInterfaceInfo(out string name, out Image icon)
 		{
 			name = "Matrix 4x4";
-			icon = Properties.Resources.B16x16_Button_Matrix_3x3;
+			icon = Properties.Resources.B16x16_Button_Matrix_4x4;
 		}
 
 		public override Size Draw(ViewInfo view, int x2, int y2)
