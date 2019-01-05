a57742c8e9865b6625243548fbe00e052e8012c7
KN4CK3R <admin@oldschoolhack.me>
2019-01-05 23:48:28 +0100

    Removed unused comment.

--- ReClass.NET/Nodes/ClassInstanceArrayNode.cs
+++ ReClass.NET/Nodes/ClassInstanceArrayNode.cs
@@ -22,11 +22,6 @@ namespace ReClassNET.Nodes
 			InnerNode.Intialize();
 		}
 
-		/// <summary>Draws this node.</summary>
-		/// <param name="view">The view information.</param>
-		/// <param name="x">The x coordinate.</param>
-		/// <param name="y">The y coordinate.</param>
-		/// <returns>The pixel size the node occupies.</returns>
 		public override Size Draw(ViewInfo view, int x, int y)
 		{
 			return Draw(view, x, y, "Array", HotSpotType.ChangeClassType);
