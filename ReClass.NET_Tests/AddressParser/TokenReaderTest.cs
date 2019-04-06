c6f771baa5dda7d684c5478f0e8a617583808c53
KN4CK3R <admin@oldschoolhack.me>
2019-04-06 23:51:36 +0200

    Added test project.

---
+++ ReClass.NET_Tests/AddressParser/TokenReaderTest.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using NFluent;
+using ReClassNET.AddressParser;
+using Xunit;
+
+namespace ReClass.NET_Tests.AddressParser
+{
+	public class TokenReaderTest
+	{
+		[Theory]
+		[InlineData("", Token.None)]
+		[InlineData("0", Token.Number)]
+		[InlineData("1", Token.Number)]
+		[InlineData("0x0", Token.Number)]
+		[InlineData("0x1", Token.Number)]
+		[InlineData("00000000", Token.Number)]
+		[InlineData("0x00000000", Token.Number)]
+		[InlineData("+", Token.Add)]
+		[InlineData("-", Token.Subtract)]
+		[InlineData("*", Token.Multiply)]
+		[InlineData("/", Token.Divide)]
+		[InlineData("(", Token.OpenParenthesis)]
+		[InlineData(")", Token.CloseParenthesis)]
+		[InlineData("[", Token.OpenBrackets)]
+		[InlineData("]", Token.CloseBrackets)]
+		[InlineData(",", Token.Comma)]
+		[InlineData("<test.exe>", Token.Identifier)]
+		public void ReadBasicToken(string formula, Token type)
+		{
+			var tokenizer = new Tokenizer(new StringReader(formula));
+
+			Check.That(tokenizer.Token).IsEqualTo(type);
+		}
+	}
+}
---
+++ ReClass.NET_Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ReClass.NET_Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ReClass.NET_Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e2d0424d-738f-41c3-9935-1b282624600f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
