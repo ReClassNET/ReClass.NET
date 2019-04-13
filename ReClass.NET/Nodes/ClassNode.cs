ReClassNET/ReClass.NET

commit d3777343a77f1125b665a242c44dc90e6f0014a5

Use int instead of IntPtr. Added IntPtr address for ClassNode.

ReClass.NET/Nodes/ClassNode.cs
@@ -39,12 +39,15 @@ namespace ReClassNET.Nodes
 			}
 		}
 
+		private IntPtr address;
+
 		public IntPtr Address
 		{
+			get => address;
 			set
 			{
 				Contract.Ensures(AddressFormula != null);
-
+				address = value;
 				AddressFormula = value.ToString("X");
 			}
 		}
@@ -191,7 +194,7 @@ namespace ReClassNET.Nodes
 
 			if (spot.Id == 0)
 			{
-				Offset = spot.Memory.Process.ParseAddress(spot.Text);
+				Address = spot.Memory.Process.ParseAddress(spot.Text);
 
 				AddressFormula = spot.Text;
 			}
@@ -203,11 +206,11 @@ namespace ReClassNET.Nodes
 
 			try
 			{
-				Offset = process.ParseAddress(AddressFormula);
+				Address = process.ParseAddress(AddressFormula);
 			}
 			catch (ParseException)
 			{
-				Offset = IntPtr.Zero;
+				Address = IntPtr.Zero;
 			}
 		}
 

ReClass.NET/CodeGenerator/CSharpCodeGenerator.cs
@@ -171,7 +171,7 @@ namespace ReClassNET.CodeGenerator
 				if (type != null)
 				{
 					writer.Write("[FieldOffset(");
-					writer.Write(node.Offset.ToInt32());
+					writer.Write(node.Offset);
 					writer.WriteLine(")]");
 
 					writer.Write("public ");
@@ -179,7 +179,7 @@ namespace ReClassNET.CodeGenerator
 					writer.Write(" ");
 					writer.Write(node.Name);
 					writer.Write("; //0x");
-					writer.Write($"{node.Offset.ToInt32():X04}");
+					writer.Write($"{node.Offset:X04}");
 					if (!string.IsNullOrEmpty(node.Comment))
 					{
 						writer.Write(" ");

ReClass.NET/CodeGenerator/CppCodeGenerator.cs
@@ -368,7 +368,7 @@ namespace ReClassNET.CodeGenerator
 			{
 				var node = new ArrayNode
 				{
-					Offset = (IntPtr)offset,
+					Offset = offset,
 					Count = count,
 					Name = $"pad_{offset:X04}"
 				};
@@ -384,7 +384,7 @@ namespace ReClassNET.CodeGenerator
 				{
 					if (fill == 0)
 					{
-						fillStart = member.Offset.ToInt32();
+						fillStart = member.Offset;
 					}
 					fill += member.MemorySize;
 
@@ -437,7 +437,7 @@ namespace ReClassNET.CodeGenerator
 				writer.Write(" ");
 				writer.Write(node.Name);
 				writer.Write("; //0x");
-				writer.Write($"{node.Offset.ToInt32():X04}");
+				writer.Write($"{node.Offset:X04}");
 				if (!string.IsNullOrEmpty(node.Comment))
 				{
 					writer.Write(" ");
@@ -449,7 +449,7 @@ namespace ReClassNET.CodeGenerator
 			{
 				writer.Write(ResolveWrappedType(node, false, logger));
 				writer.Write("; //0x");
-				writer.Write($"{node.Offset.ToInt32():X04}");
+				writer.Write($"{node.Offset:X04}");
 				if (!string.IsNullOrEmpty(node.Comment))
 				{
 					writer.Write(" ");

ReClass.NET/Forms/MainForm.Functions.cs
@@ -296,7 +296,7 @@ namespace ReClassNET.Forms
 				.WhereNot(s => s.Node is ClassNode)
 				.GroupBy(s => s.Node.GetParentContainer())
 				.SelectMany(g => g
-					.OrderBy(s => s.Node.Offset, IntPtrComparer.Instance)
+					.OrderBy(s => s.Node.Offset)
 					.GroupWhile((h1, h2) => h1.Node.Offset + h1.Node.MemorySize == h2.Node.Offset)
 				);
 
@@ -323,7 +323,7 @@ namespace ReClassNET.Forms
 					{
 						foreach (var createdNode in createdNodes)
 						{
-							hotSpotsToReplace.Enqueue(new MemoryViewControl.SelectedNodeInfo(createdNode, selected.Memory, selected.Address.Add(createdNode.Offset.Sub(node.Offset)), selected.Level));
+							hotSpotsToReplace.Enqueue(new MemoryViewControl.SelectedNodeInfo(createdNode, selected.Memory, selected.Address + createdNode.Offset - node.Offset, selected.Level));
 						}
 					}
 				}

ReClass.NET/Memory/NodeDissector.cs
@@ -30,7 +30,7 @@ namespace ReClassNET.Memory
 			Contract.Requires(node != null);
 			Contract.Requires(memory != null);
 
-			var offset = node.Offset.ToInt32();
+			var offset = node.Offset;
 			var is4ByteAligned = offset % 4 == 0;
 			var is8ByteAligned = offset % 8 == 0;
 

ReClass.NET/Nodes/ArrayNode.cs
@@ -31,9 +31,9 @@ namespace ReClassNET.Nodes
 		protected override Size DrawChild(ViewInfo view, int x, int y)
 		{
 			var v = view.Clone();
-			v.Address = view.Address.Add(Offset) + InnerNode.MemorySize * CurrentIndex;
+			v.Address = view.Address + Offset + InnerNode.MemorySize * CurrentIndex;
 			v.Memory = view.Memory.Clone();
-			v.Memory.Offset += Offset.ToInt32() + InnerNode.MemorySize * CurrentIndex;
+			v.Memory.Offset += Offset + InnerNode.MemorySize * CurrentIndex;
 
 			return InnerNode.Draw(v, x, y);
 		}

ReClass.NET/Nodes/BaseContainerNode.cs
@@ -44,7 +44,7 @@ namespace ReClassNET.Nodes
 		/// <summary>Calculates the offset of every child node.</summary>
 		public void UpdateOffsets()
 		{
-			var offset = IntPtr.Zero;
+			var offset = 0;
 			foreach (var node in Nodes)
 			{
 				node.Offset = offset;

ReClass.NET/Nodes/BaseHexCommentNode.cs
@@ -81,7 +81,7 @@ namespace ReClassNET.Nodes
 
 				if (view.Settings.ShowCommentPluginInfo)
 				{
-					var nodeAddress = view.Address.Add(Offset);
+					var nodeAddress = view.Address + Offset;
 
 					foreach (var reader in NodeInfoReader)
 					{

commit 862f058f6e9c6c91ca3289eebeff503b75e8b731

Workaround for address formula not showing bug introduced with d377734.

ReClass.NET/Nodes/ClassNode.cs
@@ -206,7 +206,11 @@ namespace ReClassNET.Nodes
 
 			try
 			{
+				var formula = AddressFormula;
+
 				Address = process.ParseAddress(AddressFormula);
+
+				AddressFormula = formula;
 			}
 			catch (ParseException)
 			{