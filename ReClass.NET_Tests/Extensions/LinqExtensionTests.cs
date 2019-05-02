955e46eff177e0230a977f6417dfcfe97c489737
KN4CK3R <admin@oldschoolhack.me>
2019-05-02 21:31:41 +0200

    Added LinqExtensions tests. Added StringExtensions tests.

---
+++ ReClass.NET_Tests/Extensions/LinqExtensionTests.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NFluent;
+using ReClassNET.Extensions;
+using Xunit;
+
+namespace ReClass.NET_Tests.Extensions
+{
+	public class LinqExtensionTests
+	{
+		public static TheoryData<IEnumerable<int>, bool> GetTestNoneData => new TheoryData<IEnumerable<int>, bool>
+		{
+			{ new int[0], true },
+			{ new int[1], false },
+			{ Enumerable.Empty<int>(), true }
+		};
+
+		[Theory]
+		[MemberData(nameof(GetTestNoneData))]
+		public void TestNone(IEnumerable<int> sut, bool expected)
+		{
+			Check.That(sut.None()).IsEqualTo(expected);
+		}
+
+		public static TheoryData<IEnumerable<int>, Func<int, bool>, bool> GetTestNoneWithSelectorData => new TheoryData<IEnumerable<int>, Func<int, bool>, bool>
+		{
+			{ new int[0], i => false, true },
+			{ new int[0], i => true, true },
+			{ new [] { 1 }, i => i == 1, false },
+			{ new [] { 1 }, i => i != 1, true },
+			{ new [] { 1, 3, 5 }, i => i % 2 == 0, true }
+		};
+
+		[Theory]
+		[MemberData(nameof(GetTestNoneWithSelectorData))]
+		public void TestNoneWithSelector(IEnumerable<int> sut, Func<int, bool> selector, bool expected)
+		{
+			Check.That(sut.None(selector)).IsEqualTo(expected);
+		}
+
+		public static TheoryData<IEnumerable<int>, Func<int, bool>> GetTestWhereNotData => new TheoryData<IEnumerable<int>, Func<int, bool>>
+		{
+			{ new int[0], i => false },
+			{ new int[0], i => true },
+			{ new [] { 1 }, i => i == 1 },
+			{ new [] { 1, 3, 5 }, i => i % 2 == 1 }
+		};
+
+		[Theory]
+		[MemberData(nameof(GetTestWhereNotData))]
+		public void TestWhereNot(IEnumerable<int> sut, Func<int, bool> selector)
+		{
+			Check.That(sut.WhereNot(selector)).IsEmpty();
+		}
+
+		public static TheoryData<IEnumerable<int>, int, int> GetTestFindIndexData => new TheoryData<IEnumerable<int>, int, int>
+		{
+			{ new int[0], 1, -1 },
+			{ new [] { 1 }, 1, 0 },
+			{ new [] { 1 }, 2, -1 },
+			{ new [] { 1, 3, 5 }, 1, 0 },
+			{ new [] { 1, 3, 5 }, 2, -1 },
+			{ new [] { 1, 3, 5 }, 3, 1 },
+			{ new [] { 1, 3, 5 }, 4, -1 },
+			{ new [] { 1, 3, 5 }, 5, 2 }
+		};
+
+		[Theory]
+		[MemberData(nameof(GetTestFindIndexData))]
+		public void TestFindIndex(IEnumerable<int> sut, int item, int expected)
+		{
+			Check.That(sut.FindIndex(i => i == item)).IsEqualTo(expected);
+		}
+
+		public static TheoryData<IEnumerable<int>> GetTestForEachData => new TheoryData<IEnumerable<int>>
+		{
+			{ Enumerable.Empty<int>() },
+			{ Enumerable.Repeat(0, 1) },
+			{ Enumerable.Repeat(0, 2) },
+			{ Enumerable.Repeat(0, 10) }
+		};
+
+		[Theory]
+		[MemberData(nameof(GetTestForEachData))]
+		public void TestForEach(IEnumerable<int> sut)
+		{
+			var sutCpy = sut.ToList();
+
+			var counter = 0;
+			sutCpy.ForEach(_ => counter++);
+
+			Check.That(counter).IsEqualTo(sutCpy.Count);
+		}
+
+		public static TheoryData<IEnumerable<int>, Func<int, int>, IEnumerable<int>> GetTestDistinctByData => new TheoryData<IEnumerable<int>, Func<int, int>, IEnumerable<int>>
+		{
+			{ Enumerable.Empty<int>(), i => i, Enumerable.Empty<int>() },
+			{ new [] { 1 }, i => i, new [] { 1 } },
+			{ new [] { 1, 1, 1, 1 }, i => i, new [] { 1 } },
+			{ new [] { 1, 2, 3, 4 }, i => i, new [] { 1, 2, 3, 4 } },
+			{ new [] { 1, 1, 2, 2, 3, 3 }, i => i, new [] { 1, 2, 3 } },
+			{ new [] { 1, 1, 2, 2, 3, 4 }, i => i, new [] { 1, 2, 3, 4 } },
+			{ new [] { 1, 1, 2, 2, 3, 4 }, i => 0, new [] { 1 } }
+		};
+
+		[Theory]
+		[MemberData(nameof(GetTestDistinctByData))]
+		public void TestDistinctBy(IEnumerable<int> sut, Func<int, int> selector, IEnumerable<int> expected)
+		{
+			Check.That(sut.DistinctBy(selector)).IsEquivalentTo(expected);
+		}
+
+		public static TheoryData<IEnumerable<int>, IEnumerable<int>, bool> GetTestIsEquivalentToData => new TheoryData<IEnumerable<int>, IEnumerable<int>, bool>
+		{
+			{ Enumerable.Empty<int>(), Enumerable.Empty<int>(), true },
+			{ Enumerable.Empty<int>(), new int[0], true },
+			{ new [] { 1 }, new int[0], false },
+			{ new [] { 1 }, new [] { 2 }, false },
+			{ new [] { 1, 2, 3 }, new [] { 1, 2, 3 }, true },
+			{ new [] { 1, 2, 3 }, new [] { 3, 1, 2 }, true }
+		};
+
+		[Theory]
+		[MemberData(nameof(GetTestIsEquivalentToData))]
+		public void TestIsEquivalentTo(IEnumerable<int> sut, IEnumerable<int> other, bool expected)
+		{
+			Check.That(sut.IsEquivalentTo(other)).IsEqualTo(expected);
+		}
+	}
+}
---
+++ ReClass.NET_Tests/Extensions/StringExtensionTest.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NFluent;
+using ReClassNET.Extensions;
+using Xunit;
+
+namespace ReClass.NET_Tests.Extensions
+{
+	public class StringExtensionTest
+	{
+		public static TheoryData<char> GetTestIsPrintableData() => new TheoryData<char>
+		{
+			'0', '9', ' ', '\t', '\n', 'a', 'A', 'z', 'Z', '-', '_', '°', '^', '"', '\\', '\"', '&', '@', '€', '$', '|', '<', '>', ';', ',', '.', ':', '#', '*', '+', '~', '`', '´', 'ß', '?', '=', '(', ')', '[', ']', '{', '}'
+		};
+
+		[Theory]
+		[MemberData(nameof(GetTestIsPrintableData))]
+		public void TestIsPrintable(char c)
+		{
+			Check.That(c.IsPrintable()).IsTrue();
+		}
+
+		public static TheoryData<char> GetTestIsNotPrintableData() => new TheoryData<char>
+		{
+			'\u0000','\u0001', '\u0002', '\u009A','\u009B', '\u009C', '\u009D','\u009E', '\u009F'
+		};
+
+		[Theory]
+		[MemberData(nameof(GetTestIsNotPrintableData))]
+		public void TestIsNotPrintable(char c)
+		{
+			Check.That(c.IsPrintable()).IsFalse();
+		}
+
+		public static TheoryData<string, int, string> GetTestLimitLengthData() => new TheoryData<string, int, string>
+		{
+			{ string.Empty, 0, string.Empty },
+			{ string.Empty, 1, string.Empty },
+			{ "01234", 0, string.Empty },
+			{ "01234", 1, "0" },
+			{ "01234", 5, "01234" },
+			{ "01234", 10, "01234" }
+		};
+
+		[Theory]
+		[MemberData(nameof(GetTestLimitLengthData))]
+		public void TestLimitLength(string sut, int length, string expected)
+		{
+			Check.That(sut.LimitLength(length)).IsEqualTo(expected);
+		}
+
+		[Fact]
+		public void TestLimitLengthThrows()
+		{
+			Check.ThatCode(() => "".LimitLength(-1)).Throws<ArgumentOutOfRangeException>();
+		}
+
+		public static TheoryData<IEnumerable<byte>, IEnumerable<char>> GetTestInterpretAsUtf8Data() => new TheoryData<IEnumerable<byte>, IEnumerable<char>>
+		{
+			{ new byte[0], string.Empty },
+			{ new [] { (byte)'t', (byte)'e', (byte)'s', (byte)'t' }, "test".ToArray() /* https://github.com/tpierrain/NFluent/issues/299 */ }
+		};
+
+		[Theory]
+		[MemberData(nameof(GetTestInterpretAsUtf8Data))]
+		public void TestInterpretAsUtf8(IEnumerable<byte> sut, IEnumerable<char> expected)
+		{
+			Check.That(sut.InterpretAsUtf8()).ContainsExactly(expected);
+		}
+
+		public static TheoryData<IEnumerable<byte>, IEnumerable<char>> GetTestInterpretAsUtf16Data() => new TheoryData<IEnumerable<byte>, IEnumerable<char>>
+		{
+			{ new byte[0], string.Empty },
+			{ new [] { (byte)'t', (byte)0, (byte)'e', (byte)0, (byte)'s', (byte)0, (byte)'t', (byte)0 }, "test".ToArray() /* https://github.com/tpierrain/NFluent/issues/299 */ }
+		};
+
+		[Theory]
+		[MemberData(nameof(GetTestInterpretAsUtf16Data))]
+		public void TestInterpretAsUtf16(IEnumerable<byte> sut, IEnumerable<char> expected)
+		{
+			Check.That(sut.InterpretAsUtf16()).ContainsExactly(expected);
+		}
+
+		public static TheoryData<IEnumerable<char>, float> GetTestCalculatePrintableDataThresholdData() => new TheoryData<IEnumerable<char>, float>
+		{
+			{ new char[0], 0.0f },
+			{ new [] { '\0' }, 0.0f },
+			{ new [] { 'a' }, 1.0f },
+			{ new [] { '\0', 'a' }, 0.0f },
+			{ new [] { 'a', '\0' }, 0.5f },
+			{ new [] { '\0', 'a', 'a' }, 0.0f },
+			{ new [] { 'a', 'a', '\0' }, 2 / 3.0f },
+			{ new [] { 'a', 'a', '\0', '\0' }, 0.5f },
+			{ new [] { 'a', 'a', '\0', '\0', '\0' }, 2 / 5.0f }
+		};
+
+		[Theory]
+		[MemberData(nameof(GetTestCalculatePrintableDataThresholdData))]
+		public void TestCalculatePrintableDataThreshold(IEnumerable<char> sut, float expected)
+		{
+			Check.That(sut.CalculatePrintableDataThreshold()).IsCloseTo(expected, 0.001);
+		}
+
+		[Theory]
+		[InlineData('a')]
+		[InlineData('a', 'a')]
+		[InlineData('a', 'a', 'f')]
+		[InlineData('#', '+', 'r', '?', 'ß', '%', '&', '§', '_', '0', '/', '(', 'ö')]
+		public void TestIsPrintableData(params char[] sut)
+		{
+			Check.That(sut.IsPrintableData()).IsTrue();
+		}
+
+		[Theory]
+		[InlineData]
+		[InlineData('a', '\0')]
+		[InlineData('\0', 'a')]
+		[InlineData('a', 'a', '\0')]
+		[InlineData('a', 'a', 'f', '\0')]
+		[InlineData('a', 'a', '\0', 'f')]
+		[InlineData('a', '\0', 'a', 'f')]
+		[InlineData('\0', 'a', 'a', 'f')]
+		public void TestIsNotPrintableData(params char[] sut)
+		{
+			Check.That(sut.IsPrintableData()).IsFalse();
+		}
+
+		[Theory]
+		[InlineData('a', 'a', 'f', '\0')]
+		[InlineData('1', '2', '3', '4', '5', '6', '7', '8', '\0', '\0')]
+		[InlineData('1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'a', 'b', '\0', '\0', '\0', '\0')]
+		public void TestIsLikelyPrintableData(params char[] sut)
+		{
+			Check.That(sut.IsLikelyPrintableData()).IsTrue();
+		}
+
+		[Theory]
+		[InlineData]
+		[InlineData('a', '\0')]
+		[InlineData('\0', 'a')]
+		[InlineData('a', 'a', '\0')]
+		[InlineData('a', 'a', '\0', 'f')]
+		[InlineData('a', 'a', '\0', '\0')]
+		[InlineData('a', '\0', 'a', 'f')]
+		public void TestIsNotLikelyPrintableData(params char[] sut)
+		{
+			Check.That(sut.IsPrintableData()).IsFalse();
+		}
+	}
+}
