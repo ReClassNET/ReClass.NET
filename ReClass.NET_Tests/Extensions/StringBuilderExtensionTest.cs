8637de556a3beb0a13c49fa9514cd1ab7a3fb2f1
KN4CK3R <admin@oldschoolhack.me>
2019-05-04 16:34:53 +0200

    Added tests for StringBuilderExtension and StringReaderExtension.

---
+++ ReClass.NET_Tests/Extensions/StringBuilderExtensionTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using NFluent;
+using ReClassNET.Extensions;
+using Xunit;
+
+namespace ReClass.NET_Tests.Extensions
+{
+	public class StringBuilderExtensionTest
+	{
+		[Fact]
+		public void TestPrependChar()
+		{
+			var sut = new StringBuilder("test");
+			sut.Prepend('x');
+
+			Check.That(sut.ToString()).IsEqualTo("xtest");
+		}
+
+		[Theory]
+		[InlineData(null)]
+		[InlineData("")]
+		[InlineData("test")]
+		public void TestPrependString(string value)
+		{
+			var sut = new StringBuilder("test");
+			sut.Prepend(value);
+
+			Check.That(sut.ToString()).IsEqualTo(value + "test");
+		}
+	}
+}
---
+++ ReClass.NET_Tests/Extensions/StringReaderExtensionTest.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using NFluent;
+using ReClassNET.Extensions;
+using Xunit;
+
+namespace ReClass.NET_Tests.Extensions
+{
+	public class StringReaderExtensionTest
+	{
+		[Theory]
+		[InlineData("", -1)]
+		[InlineData(" ", -1)]
+		[InlineData("\t", -1)]
+		[InlineData("\r", -1)]
+		[InlineData("\n", -1)]
+		[InlineData("x", (int)'x')]
+		[InlineData("x ", (int)'x')]
+		[InlineData(" x", (int)'x')]
+		[InlineData("  x", (int)'x')]
+		[InlineData("\tx ", (int)'x')]
+		[InlineData("\rx ", (int)'x')]
+		[InlineData("\nx ", (int)'x')]
+		public void TestReadSkipWhitespaces(string input, int expected)
+		{
+			using (var sut = new StringReader(input))
+			{
+				Check.That(sut.ReadSkipWhitespaces()).IsEqualTo(expected);
+			}
+		}
+	}
+}
