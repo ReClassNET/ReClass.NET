ebce22ced12258e8ec6460c5404218e6be42243f
KN4CK3R <admin@oldschoolhack.me>
2019-05-10 11:29:56 +0200

    Implemented test.

--- ReClass.NET_Tests/Extensions/ListExtensionTest.cs
+++ ReClass.NET_Tests/Extensions/ListExtensionTest.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using NFluent;
 using ReClassNET.Extensions;
 using Xunit;
@@ -13,9 +10,15 @@ namespace ReClass.NET_Tests.Extensions
 	{
 		public static TheoryData<IList<int>, Func<int, int>, int> GetTestBinarySearchData() => new TheoryData<IList<int>, Func<int, int>, int>
 		{
-
+			{ new List<int> { 0, 2, 4, 6, 8, 10, 12 }, i => 2.CompareTo(i), 1 },
+			{ new List<int> { 0, 2, 4, 6, 8, 10, 12 }, i => 8.CompareTo(i), 4 },
+			{ new List<int> { 1, 3, 5, 7, 9, 11, 13 }, i => 1.CompareTo(i), 0 },
+			{ new List<int> { 1, 3, 5, 7, 9, 11, 13 }, i => 2.CompareTo(i), ~1 },
+			{ new List<int> { 1, 3, 5, 7, 9, 11, 13 }, i => 14.CompareTo(i), ~7 },
 		};
 
+		[Theory]
+		[MemberData(nameof(GetTestBinarySearchData))]
 		public void TestBinarySearch(IList<int> sut, Func<int, int> comparer, int expected)
 		{
 			Check.That(sut.BinarySearch(comparer)).IsEqualTo(expected);
